90852489a13139c770a2749787b652ad2d76a88f
Amit Kumar Das <amit.inverse@gmail.com>
2024-05-08 23:01:22 +0530

    counter

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:state_bloc/blocs/counter/counter_bloc.dart';
 import 'package:state_bloc/equatable_testing.dart';
+import 'package:state_bloc/screens/counter_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +13,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return BlocProvider(
+      create: (context) => CounterBloc(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        // home: const EquatableTesting(),
+        home: const CounterScreen(),
       ),
-      home: const EquatableTesting(),
     );
   }
 }
